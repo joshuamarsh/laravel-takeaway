99c1028dc8934c432891455930a9b9c375ddf99b
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-04 14:56:08 +0100

    add header

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,10 +4,21 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
   <title>@yield('title')</title>
 </head>
 <body>
-  
+  <header>
+    <nav class="navmenu">
+      <a href="/"><img class="logo" src="/images/logo.png" alt=""></a>
+      <div class="navspacer"></div>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/order">Order</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
   @yield('content')
 
 </body>
