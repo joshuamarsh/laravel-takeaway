333daee851375602679d01dee66ceaf76cd93177
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-05 18:27:03 +0100

    link javascript file

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+  <link src="{{ mix('/js/app.js') }}">
   <title>@yield('title')</title>
 </head>
 <body>
