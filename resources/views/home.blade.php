8a124a85c893c7aa4faa929424a89a33426aa944
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-05 18:27:56 +0100

    first test of carousel of showcase

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,26 @@
 @section('title', 'Homepage')
 
 @section('content')
+  <div id="showcase">
+    <div><img class="showitem" src="images/showcase1.png" alt=""></div>
+    <div><img class="showitem" src="images/showcase2.png" alt=""></div>
+    <div><img class="showitem" src="images/showcase3.png" alt=""></div>
+  </div>
+  <script>
+    var myIndex = 0;
+    carousel();
     
-  
+    function carousel() {
+      var i;
+      var x = document.getElementsByClassName("showitem");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+      }
+      myIndex++;
+      if (myIndex > x.length) {myIndex = 1}    
+      x[myIndex-1].style.display = "block";  
+      setTimeout(carousel, 2000);
+    }
+  </script>
 
 @endsection
