1bddeb6a2b65e6edd36496467a83f08d68460c72
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-09 22:57:17 +0100

    added add new item function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Item;
+
 
 class HomeController extends Controller
 {
-    
     public function index()
     {
-        return view('home');
-    }
+        $items = Item::all();
 
+        return view('home', [
+            'items' => $items
+        ]);
+    }
+    public function addItem()
+    {
+
+        $item = new Item();
+
+        $item->name = request('name');
+        $item->price = request('price');
+        $item->category = request('category');
+        $item->hot = request('hot');
+        $item->vegetarian = request('vegetarian');
+
+        $item->save();
+ 
+        return redirect('/');
+    }
 }
