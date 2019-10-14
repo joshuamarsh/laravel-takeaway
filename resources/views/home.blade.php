9a99c18376206161972d08cd008875cfd556ae7e
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-14 21:52:33 +0100

    add dark overlay to background

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
 
 @section('content')
   <div id="showcase">
+    <div class="overlay"></div>
+    <div class="title"><h1>Phukhet Chinese & Thai</h1></div>
     <div><img class="showitem" src="images/showcase1.png" alt=""></div>
     <div><img class="showitem" src="images/showcase2.png" alt=""></div>
     <div><img class="showitem" src="images/showcase3.png" alt=""></div>
