9cb69d6c4ff14c7dd6d0ab55888e6438946a9299
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-14 21:50:52 +0100

    add register view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+
+@section('title', 'Register')
+
+@section('content')
+    <div id="showcase">
+        <div class="overlay"></div>
+    </div>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+        <label for="name" ></label>
+        <input id="name" type="text" name="name" placeholder="Name" required autocomplete="name" autofocus>
+        <label for="email" ></label>
+        <input id="email" type="email" name="email" placeholder="Email" required autocomplete="email">
+        <label for="password"></label>
+        <input type="password" name="password" placeholder="Password" required autocomplete="new-password">     
+        <label for="password-confirm"></label>
+        <input type="password" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">   
+        <button type="submit">
+                    Register
+        </button>
+    </form>
+@endsection
