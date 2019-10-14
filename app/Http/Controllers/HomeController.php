0f00be38df40f69c07cdc8d4347625ab8efe9c52
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-14 01:14:14 +0100

    add login page to controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,6 +16,10 @@ class HomeController extends Controller
             'items' => $items
         ]);
     }
+    public function login()
+    {
+        return view('login');
+    }
     public function addItem()
     {
 
