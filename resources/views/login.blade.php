0f94b611bacc42e84618aa03f5cca6d8216751e1
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-14 01:14:48 +0100

    add basic layout for login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('title', 'Login')
+
+@section('content')
+  <div class="login">
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+  </div>
+
+@endsection
