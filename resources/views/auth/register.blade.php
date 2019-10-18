f747ca62443c608f09f1ac366ca7edd587e387f4
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-18 23:58:50 +0100

    changed page layout to be more user friendly

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,25 @@
 @extends('master')
 
-@section('title', 'Register')
+@section('title', 'Login')
 
 @section('content')
-    <div id="showcase">
-        <div class="overlay"></div>
+    <div class="main-content">
+        <div class="login">
+            <h1>Register</h1>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <label for="name" ></label>
+                <input id="name" type="text" name="name" placeholder="Name" required autocomplete="name" autofocus>
+                <label for="email" ></label>
+                <input id="email" type="email" name="email" placeholder="Email" required autocomplete="email">
+                <label for="password"></label>
+                <input type="password" name="password" placeholder="Password" required autocomplete="new-password">     
+                <label for="password-confirm"></label>
+                <input type="password" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">   
+                <button type="submit">
+                            Register
+                </button>
+            </form>
+        </div>
     </div>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-        <label for="name" ></label>
-        <input id="name" type="text" name="name" placeholder="Name" required autocomplete="name" autofocus>
-        <label for="email" ></label>
-        <input id="email" type="email" name="email" placeholder="Email" required autocomplete="email">
-        <label for="password"></label>
-        <input type="password" name="password" placeholder="Password" required autocomplete="new-password">     
-        <label for="password-confirm"></label>
-        <input type="password" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">   
-        <button type="submit">
-                    Register
-        </button>
-    </form>
 @endsection
