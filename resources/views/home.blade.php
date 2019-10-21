ef68f5114f4b4285fa079cfc050a674e2c8a479b
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-21 02:13:34 +0100

    moved javascript to app.js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,21 +11,5 @@
     <div><img class="showitem" src="images/showcase3.png" alt=""></div>
   </div>
   
-  <script>
-    var myIndex = 0;
-    carousel();
-    
-    function carousel() {
-      var i;
-      var x = document.getElementsByClassName("showitem");
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-      }
-      myIndex++;
-      if (myIndex > x.length) {myIndex = 1}    
-      x[myIndex-1].style.display = "block";  
-      setTimeout(carousel, 3000);
-    }
-  </script>
 
 @endsection
