9dbb43ad180c5b20a5bbea416e8d10c07eedd38b
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-24 03:39:22 +0100

    added category nav bar

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -4,7 +4,22 @@
 
 @section('content')
   <div class="main-content">
-    
+    <div class="order-nav">
+      <ul>
+        <li>
+          <a href="/order/starters">Starters</a>
+        </li>
+        <li>
+          <a href="/order/mains">Mains</a>
+        </li>
+        <li>
+          <a href="/order/sides-extras">Sides & Extras </a>
+        </li>
+        <li>
+          <a href="/order/desserts">Desserts</a>
+        </li>
+      </ul>
+    </div>
   </div>
 
 @endsection
