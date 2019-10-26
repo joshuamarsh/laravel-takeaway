82fb0c79f2241c62cf552cf45501988d2eb8d6e8
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-26 05:22:26 +0100

    add starter menu view example view

---
+++ resources/views/menu/starter.blade.php
@@ -0,0 +1,17 @@
+@extends('/menu/order-master')
+
+@section('title', 'Starters')
+
+@section('items')
+  <div class="item">
+    <div class="item-name">
+        <p>Spring Rolls</p>
+    </div>
+    <div class="item-price">
+        <p>
+          <span>&pound;3.95</span>
+        </p>
+    </div>
+    <button class="item-add">ADD</button>
+  </div>
+@endsection
