171604f3c4606f9ba5e90544fcd35c694d60bbf2
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-26 05:23:17 +0100

    add order starter route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/order', 'OrderController@index');
 Route::post('/additem', 'HomeController@addItem');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/order/starters', 'OrderController@starter');
