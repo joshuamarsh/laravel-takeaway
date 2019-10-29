a620f84f14b34b88d837b3089711511d9730f059
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-29 01:10:23 +0000

    add function to add item from post request

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Item;
 use Illuminate\Http\Request;
 
 class StockController extends Controller
@@ -9,5 +10,17 @@ class StockController extends Controller
     public function index()
     {
         return view("stock");
+    }
+    public function additem(Request $itemdata)
+    {
+        Item::create([
+            'itemname' => $itemdata->name,
+            'price' => $itemdata->price,
+            'category' => $itemdata->category,
+            'hot' => $itemdata->hot,
+            'vegetarian' => $itemdata->vegetarian
+        ]);
+
+        return redirect('/stock');
     }
 }
