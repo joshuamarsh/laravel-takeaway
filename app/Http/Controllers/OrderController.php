646a11f12722ade0ca3622a82988f4d72c53a7fc
Joshua Marsh <joshuamarsh07@outlook.com>
2019-10-31 20:35:02 +0000

    order is created opening order page if not created

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Order;
+use Auth;
 
 class OrderController extends Controller
 {
@@ -13,7 +15,26 @@ class OrderController extends Controller
     }
     public function starter()
     {
+        if (Order::where('userID', Auth::user()->id)->first())
+        {
+
+        }
+        else
+        {
+            Order::create([
+                'userID' => Auth::user()->id
+            ]);
+        }
         $items = DB::select('select * from items where category="starter"');
         return view('/menu/starter')->with('items', $items);
+    }
+    public function addtobasket(Request $itemdata)
+    {
+        $orderid = Order::where('userID', Auth::user()->id)->value('orderID');
+        OrderItem::create([
+            'itemID' => $itemdata->itemid,
+            'orderID' => Auth::user()->id,
+            'quantity' => $itemdata->quantity
+        ]);
     }
 }
