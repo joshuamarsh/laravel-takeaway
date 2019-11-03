fb77795ef697f9c7c2fc50ac4c264ec205378a0c
Joshua Marsh <joshuamarsh07@outlook.com>
2019-11-03 21:20:54 +0000

    add order type column to orders table

--- database/migrations/2019_10_31_031436_create_orders_table.php
+++ database/migrations/2019_10_31_031436_create_orders_table.php
@@ -16,6 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('userID');
+            $table->string('type');
             $table->timestamps();
         });
     }
